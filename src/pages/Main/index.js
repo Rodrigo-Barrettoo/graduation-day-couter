8922f343719cb06307a1ba39c989dbae36563a3d
Rodrigo-Barrettoo <rodrigonarrettoo@outlook.com>
2020-11-23 12:58:38 -0300

    ajuste na hora

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -4,7 +4,7 @@ import React, { useState, useCallback } from 'react';
 import { Container, Content } from './styles';
 
 function Main() {
-  const dayGraduation = new Date("Nov 23 2020 00:00:00");
+  const dayGraduation = new Date("Nov 23 2020 23:59:59");
   const [days, setDays] = useState();
   const [hours, setHours] = useState();
   const [minutes, setMinutes] = useState();
